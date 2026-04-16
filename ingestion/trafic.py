58d95830b6ed734d1548f6c339819aae8d1939df
Mahu2121 <hugogbmahu@gmail.com>
2026-04-16 14:10:26 +0200

    feat(trafic): hacer pruebas de resultados

--- ingestion/trafic.py
+++ ingestion/trafic.py
@@ -9,3 +9,14 @@ data = response.json()
 
 print(data.keys())
 print(data)
+
+
+def skeleton(obj, indent=0):
+    if isinstance(obj, dict):
+        for key in obj:
+            print("  " * indent + str(key))
+            skeleton(obj[key], indent + 1)
+    elif isinstance(obj, list) and obj:
+        skeleton(obj[0], indent)
+
+skeleton(data)
