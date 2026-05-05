2fd19081d87e16c41052b4f9c5a8b0bcc8cbd874
Mahu2121 <hugogbmahu@gmail.com>
2026-05-05 21:52:02 +0200

    feat(__init__): corregir las importaciones y añadir las nuevas de stops y stop_times

--- ingestion/__init__.py
+++ ingestion/__init__.py
@@ -1,14 +1,6 @@
-from parkingOcupation import (
-    parse_parkings,
-    parkings_a_dataframe,
-    parkings_a_geodataframe,
-    asignar_parkings_a_paradas,
-)
-from trafic import (
-    parse_trafico,
-    trafico_a_dataframe,
-    trafico_a_geodataframe,
-    asignar_trafico_a_paradas,
-)
-from weather import get_aemet_weather, AEMETIngestion, HourlyWeather
-from ingest_all import get_ingestion_data
+from .parkingOcupation import parse_parkings, parkings_a_dataframe
+from .trafic import parse_trafico, trafico_a_dataframe
+from .weather import get_aemet_weather
+from .stops import load_stops
+from .ingest_all import get_ingestion_data
+from .stop_times import load_stop_times, build_stop_features
