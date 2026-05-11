1236bda284ff53b4ed31dc675ac21ac411f9ea53
Mahu2121 <hugogbmahu@gmail.com>
2026-05-11 22:43:54 +0200

    chore(train_model): quitar import inusado

--- training/train_model.py
+++ training/train_model.py
@@ -1,5 +1,4 @@
 import pandas as pd
-import numpy as np
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_absolute_error, r2_score
